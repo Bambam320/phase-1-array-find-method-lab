3dcd7ab3ecbffb0bc985612d464ffade6ace4a56
Igor Mestechkin <Igor.mestechkin@gmail.com>
2022-02-18 16:18:15 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// code your solution here
+function superbowlWin(arr) {
+  const found = arr.find((element) => element.result === "W");
+  return (found === undefined? undefined : found.year)
+  };
